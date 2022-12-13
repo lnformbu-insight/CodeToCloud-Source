1d5ba27c4cc94f392b3e116e3a234ef8b64de717
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-13 20:05:12 +0000

    updated deploy-infra file

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,26 +1,31 @@
 param
 (
-    [string] $studentprefix = "tst"
+    [string] $studentprefix = "lnt"
 )
 
-$resourcegroupName = "fabmedical-rg-" + $studentprefix
-$cosmosDBName = "fabmedical-cdb-" + $studentprefix
-$webappName = "fabmedical-web-" + $studentprefix
-$planName = "fabmedical-plan-" + $studentprefix
-$location1 = "westeurope"
-$location2 = "northeurope"
-$appInsights = "fabmedicalai-" + $studentsuffix
+$studentsuffix = "lnt"
+$resourcegroupName = "fabmedical-rg-" + $studentsuffix
+$cosmosDBName = "fabmedical-cdb-" + $studentsuffix
+$webappName = "fabmedical-web-" + $studentsuffix
+$planName = "fabmedical-plan-" + $studentsuffix
+$workspaceName = "fabmedical-law-" + $studentsuffix
+$appInsights = "fabmedical-ai-" + $studentsuffix
+$location1 = "westus3"
+$location2 = "eastus"
+$dbConnection = ""
+$manipulate = ""
+$dbKeys = ""
 
 #First create a group
 $rg = az group create --name $resourcegroupName --location $location1 | ConvertFrom-Json 
 
-#Then create a CosmosDB
+#create the cosmosDB with 2 failover locations
 az cosmosdb create --name $cosmosDBName `
 --resource-group $resourcegroupName `
 --locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
 --locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
 --enable-multiple-write-locations `
---kind MongoDB
+--kind MongoDB `
 
 #Create a Azure App Service Plan
 az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
@@ -28,7 +33,6 @@ az appservice plan create --name $planName --resource-group $resourcegroupName -
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_URL="https://ghcr.io" --name $($webappName) --resource-group $($resourcegroupName) 
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_USERNAME="notapplicable" --name $($webappName) --resource-group $($resourcegroupName) 
 az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_PASSWORD="$($env:CR_PAT)" --name $($webappName) --resource-group $($resourcegroupName) 
-
 
 #Create a Azure Web App with NGINX container
 az webapp create `
