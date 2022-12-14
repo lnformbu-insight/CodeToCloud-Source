3e56fcc8c8123f2a4276946125383d9f58961459
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-14 19:22:53 +0000

    updated

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,3 +1,51 @@
+$script:studentprefix = "ltn"
+$script:resourcegroupName = "fabmedical-rg-" + $studentsuffix
+$script:webappName = "fabmedical-web-" + $studentsuffix
+$script:workspaceName = "fabmedical-law-" + $studentsuffix
+$script:location1 = "westus3"
+$script:CR_PAT = $CR_PAT
+
+#login to github
+git config --global user.email "lnformbu-insight@insight.com"
+git config --global user.name "Lenon Nformbui"
+
+#Create the following:
+#log analytics workspace: fabmedical-law-add / app insights: fabmedical-ai-add
+& ./deploy-infra.ps1
+& ./deploy-ai.ps1
+#configure the app insights instrumentation key and insert it into app.js
+$insertString = "appInsights.setup(`"" + $aiInstKey + "`");"
+(Get-Content ../content-web/app.js) -Replace "appInsights\.setup\(\`"*\S*\`"*\);", $insertString | Set-Content ../content-web/app.js
+ 
+#commit the updated app.js
+& ./gitpp.ps1
+
+#re-deploy the web container to the application
+& ./deploy-container.ps1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<#
 #All  variables for infra build
 $studentsuffix = "lnt"
 $resourcegroupName = "fabmedical-rg-" + $studentsuffix
@@ -98,3 +146,4 @@ az webapp config appsettings set --resource-group $resourceGroupName `
     XDT_MicrosoftApplicationInsights_PreemptSdk=disabled `
     WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
 
+#<
