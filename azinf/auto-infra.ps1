88a50c1409be559349ea2d42462ab3183efae2b3
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-14 18:52:17 +0000

    updated

--- azinf/auto-infra.ps1
+++ azinf/auto-infra.ps1
@@ -11,16 +11,16 @@ git config --global user.name "Lenon Nformbui"
 
 #Create the following:
 #log analytics workspace: fabmedical-law-add / app insights: fabmedical-ai-add
-& .\deploy-infra.ps1
-& .\deploy-ai.ps1
+& ./deploy-infra.ps1
+& ./deploy-ai.ps1
 #configure the app insights instrumentation key and insert it into app.js
 $insertString = "appInsights.setup(`"" + $aiInstKey + "`");"
 (Get-Content ../content-web/app.js) -Replace "appInsights\.setup\(\`"*\S*\`"*\);", $insertString | Set-Content ../content-web/app.js
  
 #commit the updated app.js
-& .\gitpp.ps1
+& ./gitpp.ps1
 
 #re-deploy the web container to the application
-& .\deploy-container.ps1
+& ./deploy-container.ps1
 
 
