c8b5133a7ad82448e2432584a9dfcfd1b207f258
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-14 18:46:41 +0000

    change path

--- azinf/auto-infra.ps1
+++ azinf/auto-infra.ps1
@@ -5,7 +5,9 @@ $script:workspaceName = "fabmedical-law-" + $studentsuffix
 $script:location1 = "westus3"
 $script:CR_PAT = $CR_PAT
 
-
+#login to github
+git config --global user.email "lnformbu-insight@insight.com"
+git config --global user.name "Lenon Nformbui"
 
 #Create the following:
 #log analytics workspace: fabmedical-law-add / app insights: fabmedical-ai-add
@@ -16,7 +18,7 @@ $insertString = "appInsights.setup(`"" + $aiInstKey + "`");"
 (Get-Content ../content-web/app.js) -Replace "appInsights\.setup\(\`"*\S*\`"*\);", $insertString | Set-Content ../content-web/app.js
  
 #commit the updated app.js
-&.\gitpp.ps1
+& .\gitpp.ps1
 
 #re-deploy the web container to the application
 & .\deploy-container.ps1
