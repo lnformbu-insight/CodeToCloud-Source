a242883077cb04a00fc366831828be8331e0bee0
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-14 19:39:05 +0000

    updated files

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,49 +1,124 @@
-$script:studentprefix = "ltn"
-$script:resourcegroupName = "fabmedical-rg-" + $studentsuffix
-$script:webappName = "fabmedical-web-" + $studentsuffix
-$script:workspaceName = "fabmedical-law-" + $studentsuffix
-$script:location1 = "westus3"
-$script:CR_PAT = $CR_PAT
+#All  variables for infra build
+$studentsuffix = "ltn"
+$resourcegroupName = "fabmedical-rg-" + $studentsuffix
+$cosmosDBName = "fabmedical-cdb-" + $studentsuffix
+$webappName = "fabmedical-web-" + $studentsuffix
+$planName = "fabmedical-plan-" + $studentsuffix
+$workspaceName = "fabmedical-law-" + $studentsuffix
+$appInsights = "fabmedical-ai-" + $studentsuffix
+$location1 = "westus3"
+$location2 = "eastus"
+$dbConnection = ""
+$manipulate = ""
+$dbKeys = ""
+
 
 #login to github
 git config --global user.email "lnformbu-insight@insight.com"
 git config --global user.name "Lenon Nformbui"
 
-#Create the following:
-#log analytics workspace: fabmedical-law-add / app insights: fabmedical-ai-add
-& ./deploy-infra.ps1
-& ./deploy-ai.ps1
+
+#create the resource group
+az group create -l $location1 -n $resourcegroupName
+
+#create the cosmosDB with 2 failover locations
+az cosmosdb create --name $cosmosDBName `
+--resource-group $resourcegroupName `
+--locations regionName=$location1 failoverPriority=0 isZoneRedundant=False `
+--locations regionName=$location2 failoverPriority=1 isZoneRedundant=True `
+--enable-multiple-write-locations `
+--kind MongoDB 
+
+
+#create the App Service Plan
+az appservice plan create --name $planName --resource-group $resourcegroupName --sku S1 --is-linux
+
+#get and configure dbConnection string
+$dbKeys = az cosmosdb keys list -n $cosmosDBName -g $resourcegroupName --type connection-strings `
+    --query "connectionStrings[?description=='Primary MongoDB Connection String'].connectionString"
+$manipulate = $dbKeys[1]
+$manipulate = $manipulate.Split("""")[1]
+$manipulate = $manipulate.Split("?")
+$dbConnection = $manipulate[0] + "contentdb?" + $manipulate[1]
+
+#create the WebApp with nginx
+az webapp create --resource-group $resourcegroupName `
+--plan $planName --name $webappName -i nginx
+
+#configure the webapp settings
+az webapp config container set `
+--docker-registry-server-password $CR_PAT `
+--docker-registry-server-url https://ghcr.io `
+--docker-registry-server-user notapplicable `
+--multicontainer-config-file ../docker-compose.yml `
+--multicontainer-config-type COMPOSE `
+--name $webappName `
+--resource-group $resourcegroupName `
+--enable-app-service-storage true
+
+
+#set the mongoDB connection
+az webapp config appsettings set --resource-group $resourceGroupName `
+--name $webappName `
+--settings MONGODB_CONNECTION=$dbConnection
+
+#populate the database with content fron ghcr.io - fabrikam-init
+docker run -ti --rm -e MONGODB_CONNECTION=$dbConnection ghcr.io/lnformbu-insight/fabrikam-init
+
+
+#================================================================================================================
+$studentsuffix = "ltn"
+$resourcegroupName = "fabmedical-rg-" + $studentsuffix
+$webappName = "fabmedical-web-" + $studentsuffix
+$workspaceName = "fabmedical-law-" + $studentsuffix
+$appInsights = "fabmedical-ai-" + $studentsuffix
+$location1 = "westus3"
+
+
+
+
+#Create the following: A log analytics workspace & app insights
+az monitor log-analytics workspace create --resource-group $resourcegroupName `
+    --workspace-name $workspaceName
+
+az extension add --name application-insights
+sudo npm install applicationinsights
+$ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName `
+    --workspace "/subscriptions/c074675d-209c-429a-a95e-ea35b822e146/resourceGroups/fabmedical-rg-ltn/providers/Microsoft.OperationalInsights/workspaces/fabmedical-law-ltn" `
+    --application-type web | ConvertFrom-Json
+
+$global:aiInstKey = $ai.instrumentationKey
+$aiConnectionString = $ai.connectionString
+
+#link the newly created app insights to the webapp with default values
+az webapp config appsettings set --resource-group $resourceGroupName `
+--name $webappName `
+--settings APPINSIGHTS_INSTRUMENTATIONKEY=$global:aiInstKey `
+    APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 `
+    APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 `
+    APPLICATIONINSIGHTS_CONNECTION_STRING=$aiConnectionString `
+    ApplicationInsightsAgent_EXTENSION_VERSION=~2 `
+    DiagnosticServices_EXTENSION_VERSION=~3 `
+    InstrumentationEngine_EXTENSION_VERSION=disabled `
+    SnapshotDebugger_EXTENSION_VERSION=disabled `
+    XDT_MicrosoftApplicationInsights_BaseExtensions=disabled `
+    XDT_MicrosoftApplicationInsights_Mode=recommended `
+    XDT_MicrosoftApplicationInsights_PreemptSdk=disabled `
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
+   
+#================================================================================================================
 #configure the app insights instrumentation key and insert it into app.js
 $insertString = "appInsights.setup(`"" + $aiInstKey + "`");"
 (Get-Content ../content-web/app.js) -Replace "appInsights\.setup\(\`"*\S*\`"*\);", $insertString | Set-Content ../content-web/app.js
  
+#================================================================================================================
 #commit the updated app.js
-& ./gitpp.ps1
+git add ../content-web/app.js
+git commit -m "updated aiInstKey in app.js"
+git push
+#================================================================================================================
 
-#re-deploy the web container to the application
-& ./deploy-container.ps1
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#================================================================================================================
 
 <#
 #All  variables for infra build
