d1709e76f72bad49998819fdcaa32a5d0b25eddd
Lenon Nformbui <116840444+lnformbu-insight@users.noreply.github.com>
2022-12-14 20:00:43 +0000

    updated suffix

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -1,5 +1,5 @@
 #All  variables for infra build
-$studentsuffix = "ltn"
+$studentsuffix = "lnt"
 $resourcegroupName = "fabmedical-rg-" + $studentsuffix
 $cosmosDBName = "fabmedical-cdb-" + $studentsuffix
 $webappName = "fabmedical-web-" + $studentsuffix
@@ -75,7 +75,7 @@ az monitor log-analytics workspace create --resource-group $resourcegroupName `
 az extension add --name application-insights
 sudo npm install applicationinsights
 $ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName `
-    --workspace "/subscriptions/c074675d-209c-429a-a95e-ea35b822e146/resourceGroups/fabmedical-rg-ltn/providers/Microsoft.OperationalInsights/workspaces/fabmedical-law-ltn" `
+    --workspace "/subscriptions/c074675d-209c-429a-a95e-ea35b822e146/resourceGroups/fabmedical-rg-lnt/providers/Microsoft.OperationalInsights/workspaces/fabmedical-law-lnt" `
     --application-type web | ConvertFrom-Json
 
 $global:aiInstKey = $ai.instrumentationKey
