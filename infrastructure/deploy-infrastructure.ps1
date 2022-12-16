449f8267bdd3297b498ba1fc67c96af8000f7606
Lenon Nformbui <lnformbu-insight@insight.com>
2022-12-16 17:11:22 +0000

    updated file

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -42,15 +42,15 @@ az webapp create --resource-group $resourcegroupName `
 
 #configure the webapp settings
 az webapp config container set `
---docker-registry-server-password $CR_PAT `
---docker-registry-server-url https://ghcr.io `
---docker-registry-server-user notapplicable `
 --multicontainer-config-file ../docker-compose.yml `
 --multicontainer-config-type COMPOSE `
 --name $webappName `
 --resource-group $resourcegroupName `
 --enable-app-service-storage true
 
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_URL="https://ghcr.io" --name $($webappName) --resource-group $($resourcegroupName) 
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_USERNAME="notapplicable" --name $($webappName) --resource-group $($resourcegroupName) 
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_PASSWORD="$($env:CR_PAT)" --name $($webappName) --resource-group $($resourcegroupName)
 
 #set the mongoDB connection
 az webapp config appsettings set --resource-group $resourceGroupName `
@@ -58,3 +58,30 @@ az webapp config appsettings set --resource-group $resourceGroupName `
 --settings MONGODB_CONNECTION=$dbConnection
 
 
+#Create the following: A log analytics workspace & app insights
+az monitor log-analytics workspace create --resource-group $resourcegroupName `
+    --workspace-name $workspaceName
+
+az extension add --name application-insights
+$ai = az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName `
+    --workspace "/subscriptions/c074675d-209c-429a-a95e-ea35b822e146/resourceGroups/fabmedical-rg-ltn/providers/Microsoft.OperationalInsights/workspaces/fabmedical-law-ltn" `
+    --application-type web | ConvertFrom-Json
+
+$global:aiInstKey = $ai.instrumentationKey
+$aiConnectionString = $ai.connectionString
+
+#link the newly created app insights to the webapp with default values
+az webapp config appsettings set --resource-group $resourceGroupName `
+--name $webappName `
+--settings APPINSIGHTS_INSTRUMENTATIONKEY=$global:aiInstKey `
+    APPINSIGHTS_PROFILERFEATURE_VERSION=1.0.0 `
+    APPINSIGHTS_SNAPSHOTFEATURE_VERSION=1.0.0 `
+    APPLICATIONINSIGHTS_CONNECTION_STRING=$aiConnectionString `
+    ApplicationInsightsAgent_EXTENSION_VERSION=~2 `
+    DiagnosticServices_EXTENSION_VERSION=~3 `
+    InstrumentationEngine_EXTENSION_VERSION=disabled `
+    SnapshotDebugger_EXTENSION_VERSION=disabled `
+    XDT_MicrosoftApplicationInsights_BaseExtensions=disabled `
+    XDT_MicrosoftApplicationInsights_Mode=recommended `
+    XDT_MicrosoftApplicationInsights_PreemptSdk=disabled `
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE=true
